8f6ca0746e5a0927a9a9d9e2ebe4a2af056e2253
Yann Irrilo <15703478+yirrilo@users.noreply.github.com>
2020-11-14 21:40:33 +0100

    feat: optimize the writer of symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataFromMap.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataFromMap.java
@@ -1,25 +1,37 @@
 package com.hemebiotech.analytics;
 
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
+/**
+ * Class used to serialize a map of symptoms counter.
+ */
 public class WriteSymptomDataFromMap implements ISymptomWriter {
 
 	private String outputFilename;
 
+	/**
+	 * Writer of symptoms having to write the result to the given output path.
+	 * @param outputFilename filename to use to write the result.
+	 */
 	public WriteSymptomDataFromMap(String outputFilename) {
 		this.outputFilename = outputFilename;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void writeSymptoms(Map<String, Integer> countedSymptoms) throws IOException {
-		FileWriter writer = new FileWriter(outputFilename);
-		for ( Entry<String, Integer> symptomEntry : countedSymptoms.entrySet()) {
-			writer.write(symptomEntry.getKey() + ": " + symptomEntry.getValue() + "\n");
-		}
-		writer.close();
+		List<String> content = countedSymptoms.entrySet().stream()
+				.map( entry -> entry.getKey() + ": " + entry.getValue())
+				.collect(Collectors.toList());
+		Files.write(Path.of(outputFilename), content, StandardOpenOption.TRUNCATE_EXISTING);
 	}
 
 }
