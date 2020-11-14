5b4f346ad283b718aef110a9891c4bfd35807221
Yann Irrilo <15703478+yirrilo@users.noreply.github.com>
2020-11-14 19:28:29 +0100

    feat: Sort the result

--- Project02Eclipse/src/com/hemebiotech/analytics/CountSymptomDataFromList.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/CountSymptomDataFromList.java
@@ -1,14 +1,14 @@
 package com.hemebiotech.analytics;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CountSymptomDataFromList implements ISymptomCounter {
 
 	@Override
 	public Map<String, Integer> countSymptoms(List<String> symptoms) {
-		Map<String, Integer> countedSymptoms = new HashMap<>();
+		Map<String, Integer> countedSymptoms = new TreeMap<>();
 		for (String symptom : symptoms) {
 			System.out.println("symptom from file: " + symptom);
 			Integer count = countedSymptoms.get(symptom);
