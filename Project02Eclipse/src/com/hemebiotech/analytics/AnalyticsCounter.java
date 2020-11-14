10de58825cfa28b572f73ce3702d554211781802
Yann Irrilo <15703478+yirrilo@users.noreply.github.com>
2020-11-14 18:48:59 +0100

    feat: use a map

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,35 +1,40 @@
 package com.hemebiotech.analytics;
 
 import java.io.FileWriter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
 	
 	public static void main(String args[]) throws Exception {
 		ISymptomReader reader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
 		List<String> symptoms = reader.GetSymptoms();
 		
+		Map<String, Integer> countedSymptoms = new HashMap<>();
 		for (String symptom : symptoms) {
 			System.out.println("symptom from file: " + symptom);
 			if (symptom.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
+				Integer count = countedSymptoms.get("headache");
+				count = count == null ? 1 : count + 1;
+				countedSymptoms.put("headache", count);
 			}
 			else if (symptom.equals("rash")) {
-				rashCount++;
+				Integer count = countedSymptoms.get("rash");
+				count = count == null ? 1 : count + 1;
+				countedSymptoms.put("rash", count);
 			}
 			else if (symptom.contains("pupils")) {
-				pupilCount++;
+				Integer count = countedSymptoms.get("pupils");
+				count = count == null ? 1 : count + 1;
+				countedSymptoms.put("pupils", count);
 			}
 		}
 		
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("headache: " + countedSymptoms.get("headache") + "\n");
+		writer.write("rash: " + countedSymptoms.get("rash") + "\n");
+		writer.write("dialated pupils: " + countedSymptoms.get("pupils") + "\n");
 		writer.close();
 	}
 }
